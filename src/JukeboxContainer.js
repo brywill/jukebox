50fed3c1a8240a581b108bd47a310a784e835aef
bwilliams3 <bwilliams3@peacecorps.gov>
2018-05-20 20:10:29 -0400

    Check all page results from API

--- src/JukeboxContainer.js
+++ src/JukeboxContainer.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import SearchResults from "./SearchResults";
 
-const api = 'http://ws.audioscrobbler.com/2.0/?method=track.search&limit=1000&api_key=f7a213cd9d971aafdf285b4885f18916&format=json&track=';
-
+const api = 'http://ws.audioscrobbler.com/2.0/?method=track.search&limit=100&api_key=f7a213cd9d971aafdf285b4885f18916&format=json&track=';
+let songArray = [];
 
 class JukeboxContainer extends Component {
 	constructor(props) {
@@ -13,33 +13,45 @@ class JukeboxContainer extends Component {
 		};
 
 		this.submitRequest = this.submitRequest.bind(this);
+		this.getResults = this.getResults.bind(this);
+		this.buildSongList = this.buildSongList.bind(this);
+	}
+
+	buildSongList(tracks, q) {
+		let wordArray = [];
+		for (let i = 0; i < tracks.length; i++) {
+			wordArray = tracks[i].name.toLowerCase().split(" ");
+			if (wordArray.includes(q) && tracks[i].listeners > 10) {
+				songArray.push({
+					key: tracks[i].listeners + Math.random(),
+					title: tracks[i].name,
+					artist: tracks[i].artist,
+					playcount: tracks[i].listeners
+				});
+			}
+			wordArray = [];
+		}
+	}
+
+	getResults(q, pageNum) {	
+		fetch(api + q + '&page=' + pageNum)
+			.then(response => response.json())
+			.then(data => {
+				let tracks = data.results.trackmatches.track;
+				this.buildSongList(tracks, q);
+				if (tracks.length > 0) {
+					pageNum++;
+					this.getResults(q, pageNum);
+				} else {
+					this.setState({ songs: songArray, count: songArray.length, term: q });
+				}
+			});
 	}
 
 	submitRequest(e) {
 		e.preventDefault();
 		var q = this._inputElement.value.toLowerCase();
-
-		fetch(api + q)
-			.then(response => response.json())
-			.then(data => {
-				let tracks = data.results.trackmatches.track;
-				let songArray = [];
-				let wordArray = [];
-
-				for (let i = 0; i < tracks.length; i++) {
-					wordArray = tracks[i].name.toLowerCase().split(" ");
-					if (wordArray.includes(q) && tracks[i].listeners > 100000) {
-						songArray.push({
-							key: tracks[i].listeners,
-							title: tracks[i].name,
-							artist: tracks[i].artist,
-							playcount: tracks[i].listeners
-						});
-					}
-					wordArray = [];
-				}
-				this.setState({ songs: songArray, count: songArray.length, term: q });
-			});
+		this.getResults(q, 1);
 		this._inputElement.value = "";
 	}
 
